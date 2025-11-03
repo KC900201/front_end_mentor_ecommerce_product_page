3ab4e5ef455dc1fa85d8f03b29f41f79b1e66305
KC Ng <nkc900201@gmail.com>
2025-11-03 19:24:28 +0900

    fix: migrate head.ts to src/ folder. Update the github link for the NavBarMenu toggle issue

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -22,7 +22,7 @@ import { navigation } from "@/data/navigations"
 
 /**
  * Issue:
- *  [ ] NavbarMenuToggle doesn't trigger mobile navbar
+ *  [ ] NavbarMenuToggle doesn't trigger mobile  - https://github.com/heroui-inc/heroui/issues/4345, https://github.com/heroui-inc/heroui/issues/4393, https://github.com/tailwindlabs/tailwindcss/discussions/16369
  *  [ ] Hamburger icon doesn't display
  *
  * @returns a header component with cart button and navigation bar
