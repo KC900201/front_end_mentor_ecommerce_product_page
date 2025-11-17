0a70b7ff6cabc1a158325dd5fefb0d7a8742e4c0
KC Ng <nkc900201@gmail.com>
2025-11-17 18:14:15 +0900

    feat: updating test scripts for ProductImage and PriceDisplay components. Rename the test script for ThumbnailImage

---
+++ src/components/atoms/ThumbnailImage.test.tsx
@@ -0,0 +1,6 @@
+import { render, screen } from "@testing-library/react"
+import { afterEach, describe, expect, it, vi } from "vitest"
+
+import ThumbnailImage from "./ThumbnailImage"
+
+describe("ThumbnailImage", () => {})
